d4f0e87290a7dcab6095dcb1109b0f4788263906
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2022-07-06 15:08:24 +0300

    add search

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useMemo } from 'react';
 import Counter from './components/Counter';
 import PostForm from './components/PostForm';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import PostsFilter from './components/PostsFilter';
 import './styles/App.css';
+import MyInput from './UI/input/MyInput';
 import MySelect from './UI/select/MySelect';
 
 function App() {
@@ -13,7 +16,20 @@ function App() {
     { id: 3, title: 'уJs', body: 'description description' }
   ]);
 
-  const [selectedSort, setSelectedSort] = useState('');
+  // const [selectedSort, setSelectedSort] = useState('');
+  // const [searchQuery, setSearchQuery] = useState('');
+
+  const [filter, setFilter] = useState({ sort: '', query: '' })
+
+  const sortedPosts = useMemo(() => {
+    if (filter.sort)
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+    return posts;
+  }, [filter.sort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -23,30 +39,16 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-  }
-
   return (
     <div className="App">
       <PostForm create={createPost} />
 
       <hr style={{ margin: '15px' }} />
 
-      <MySelect
-        value={selectedSort}
-        onChange={sortPosts}
-        defaultValue="Сортировка"
-        options={[
-          { value: 'title', name: 'По названию' },
-          { value: 'body', name: 'По описанию' },
-        ]}
+      <PostsFilter filter={filter} setFilter={setFilter} />
 
-      />
-
-      {posts.length !== 0
-        ? <PostList remove={removePost} posts={posts} title='Список постовв' />
+      {sortedAndSearchedPosts.length !== 0
+        ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постовв' />
         : <div>Постов нет</div>
       }
 
